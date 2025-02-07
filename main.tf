4b96cdcb3aca3762eca374df13e1539c6b6bf8b1
kuldeep26 <kuldeepsingh260893@gmail.com>
2025-02-07 17:11:40 +0530

    fix

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "ecs_module" {
   source                 = "./ecs"
   api_role_password      = module.db_module.api_role_password
   ingestor_role_password = module.db_module.ingestor_role_password
-  insights_role_username = module.db_module.insights_role_password
+  insights_role_password = module.db_module.insights_role_password
 }
