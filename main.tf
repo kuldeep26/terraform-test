952219eaf76c0cde03a5c5d6362d264ce25df813
kuldeep26 <kuldeepsingh260893@gmail.com>
2025-02-07 17:07:42 +0530

    modules directory changes

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "db_module" {
-  source = "/db"
+  source = "./db"
 }
 
 module "ecs_module" {
-  source                 = "/ecs"
+  source                 = "./ecs"
   api_role_password      = module.db_module.api_role_password
   ingestor_role_password = module.db_module.ingestor_role_password
   insights_role_username = module.db_module.insights_role_password
